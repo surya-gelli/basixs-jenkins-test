2b8424f33de31949ecdfb9c49e18d5bef3357b5a
Coditas <surya.gelli@coditas.com>
2021-03-31 15:03:42 +0530

    updated rollback

--- changes.sh
+++ changes.sh
@@ -12,7 +12,7 @@ fi
 
 # Check is files in given directory changed between commits
 # NOTE: $GIT_PREVIOUS_COMMIT and $GIT_COMMIT provided by Jenkins GIT Plugin
-CHANGED=`git diff --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT lambdas/rollback/`
+CHANGED=`git diff --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT lambdas/`
 
 if [ -z "$CHANGED" ]; then
     echo "No changes dettected..."
